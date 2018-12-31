03ee12bdab4bd1e6110fcdae935b735ce71222f6
booldook <booldook@gmail.com>
2018-12-31 10:50:00 +0900

    첫번째 커밋

---
+++ public/js/note.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDCNWAVr_Pn778hBj4RSGBYcoXjK2IVVBA",
+	authDomain: "booldook-note2.firebaseapp.com",
+	databaseURL: "https://booldook-note2.firebaseio.com",
+	projectId: "booldook-note2",
+	storageBucket: "booldook-note2.appspot.com",
+	messagingSenderId: "63310701259"
+};
+firebase.initializeApp(config);
