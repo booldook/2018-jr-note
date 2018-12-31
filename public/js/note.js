620506777821764e50b041848f649f20a6cfcb82
booldook <booldook@gmail.com>
2018-12-31 14:12:03 +0900

    login 구현

--- public/js/note.js
+++ public/js/note.js
@@ -12,17 +12,58 @@ firebase.initializeApp(config);
 var db = firebase.database();
 var auth = firebase.auth();
 var google = new firebase.auth.GoogleAuthProvider();
+var user = null;
 
-$("#bt_google").on("click", function(){
+// 1번 방식
+/*
+$("#bt_google_login").on("click", function(){
 	auth.signInWithPopup(google).then(function(data){
-		
+		$("#bt_google_login").hide();
+		$("#bt_google_logout").show();
+		user = data.user;
+		$(".email").html(user.email);
+		$(".symbol").show();
+		$(".symbol > img").attr("src", user.photoURL);
 	});
 });
-$("#bt_logout").on("click", function(){
-	auth.signOut().then(function(){
-		log("로그아웃");
+$("#bt_google_logout").on("click", function(){
+	auth.signOut().then(function(data){
+		$(this).hide();
+		$("#bt_google_login").show();
+		$("#bt_google_logout").hide();
+		user = null;
+		$(".email").html("");
+		$(".symbol").hide();
+		$(".symbol > img").attr("src", "");
 	});
+});
+*/
+
+
+//2번 방식
+$("#bt_google_login").click(function(){
+	auth.signInWithPopup(google);
+});
+$("#bt_google_logout").click(function(){
+	auth.signOut();
 });
 auth.onAuthStateChanged(function(data){
-	log(data);
+	if(data) {
+		//signIn 상태
+		user = data;
+		$("#bt_google_login").hide();
+		$("#bt_google_logout").show();
+		$(".email").html(user.email);
+		$(".symbol").show();
+		$(".symbol > img").attr("src", user.photoURL);
+	}
+	else {
+		//signOut 상태
+		user = null;
+		$("#bt_google_login").show();
+		$("#bt_google_logout").hide();
+		$(".email").html("");
+		$(".symbol").hide();
+		$(".symbol > img").attr("src", "");
+	}
 });
